d1300443113effb08b767a68545c4e634dcb14d6
edptemp3 <129116066+RasidinHatta@users.noreply.github.com>
2024-12-04 09:54:54 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default async function Home() {
 
   return (
-    <div className="absolute top-20 w-full h-full overflow-hidden">
-      Home
+    <div className="flex items-center justify-center">
+      <Card className="w-[80%]">
+        <CardHeader>
+          <CardTitle>Home</CardTitle>
+          <CardDescription>Home Page Description</CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
     </div>
   );
 }
