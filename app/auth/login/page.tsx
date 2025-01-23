d310be7e80ea98616c6b9415a5305ca423dd13eb
RasidinHatta <129116066+RasidinHatta@users.noreply.github.com>
2025-01-23 10:13:36 +0800

    asdsad
    sad

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,94 @@
-import { SignIn } from '@/components/auth/signin-button'
-import React from 'react'
+"use client";
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { signInSchema } from '@/lib/zod';
+import LoadingButton from '@/components/loading-button';
+import { handleCredentialsSignin } from '@/app/actions/authActions';
+import ErrorMessage from '@/components/error-message';
 
 const LoginPage = () => {
+  const [globalError, setGlobalError] = useState<string>("");
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "rasidin@eraser.com",
+      password: "123456",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof signInSchema>) => {
+    try {
+      const result = await handleCredentialsSignin(values);
+      if (result?.message) {
+        setGlobalError(result.message);
+      }
+    } catch (error) {
+      console.log("An unexpected error occurred. Please try again.");
+    }
+  };
+
   return (
-    <div className="flex items-center justify-center h-full">
-      <SignIn />
+    <div className="flex items-center justify-center ">
+      <Card className='w-full max-w-md'>
+        <CardHeader>
+          <CardTitle className='text-3xl font-bold text-center'>
+            Welcome Back
+          </CardTitle>
+          <CardContent>
+            {globalError && <ErrorMessage error={globalError} />}
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          placeholder="Enter your email address"
+                          autoComplete="off"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="Enter password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <LoadingButton pending={form.formState.isSubmitting} />
+
+              </form>
+            </Form>
+          </CardContent>
+        </CardHeader>
+      </Card>
     </div>
   )
 }
